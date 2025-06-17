09a7cecf2e236b1b28e44fb715759d958be478a1
MuriloIA <musilva.14@gmail.com>
2025-06-17 16:03:13 -0300

    Primeira versão v0.1

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ st.set_page_config(
 def load_dataset():
     # Carregando os dados
     df = pd.read_csv('dataset/gapminder_data_graphs.csv')
+
+    # Removendo as linhas com valores NaN
+    df = df.dropna()
+
     # Retorna os dados
     return df
 df = load_dataset()
@@ -24,7 +28,7 @@ st.sidebar.divider()
 
 # Seleção do Ano
 unique_years = df['year'].unique()
-year_select  = st.selectbox(label='Ano', options=unique_years)
+year_select  = st.selectbox(label='Ano', options=sorted(unique_years))
 
 # Filtrados o dataset 
 df_filtred = df[df['year'] == year_select]
@@ -32,26 +36,43 @@ df_filtred = df[df['year'] == year_select]
 # Continents
 unique_continentes = ['Todos'] + df_filtred['continent'].unique().tolist()
 continente_select  = st.sidebar.selectbox(label='Continente', options=unique_continentes)
-
-
+title = 'Expectativa de vida em relação ao PIB per capita por continente'
 # Filtrando novamente por continente
 if continente_select != 'Todos':
     df_filtred = df_filtred[df_filtred['continent'] == continente_select]
+
+    # Filtrando por país
+    countrys = ['Todos'] + df_filtred['country'].unique().tolist()
+    country  = st.sidebar.selectbox(label='País', options=countrys)
+    if country != 'Todos':
+        df_filtred = df_filtred[df_filtred['country'] == country]
+        title = f'Expectativa de vida em relação ao PIB per capita no {country}'
+        labelc1 = 'Expectativa de Vida'
+        labelc2 = 'PIB per capita'
+    else:
+        labelc1 = 'Expectativa de Vida Média'
+        labelc2 = 'PIB per capita Média'
 else:
-    pass
+    labelc1 = 'Expectativa de Vida Média'
+    labelc2 = 'PIB per capita Média'
 
 # Indicadores globais
-c1, c2, c3 = st.columns(3)
-c1.metric(label='Expectativa de Vida Média', value=f"{df_filtred['life_exp'].mean():.2f}")
-c2.metric(label='Índice de Desenvolvimento Humano (IDH)', value=f"{df_filtred['hdi_index'].mean():.2f}")
-c3.metric(label='PIB per capita', value=f"{df_filtred['hdi_index'].mean():.2f}")
+c1, c2 = st.columns(2)
+c1.metric(label=labelc1, value=f"{df_filtred['life_exp'].mean():.2f}")
+# c2.metric(label='Índice de Desenvolvimento Humano (IDH) Médio', value=f"{df_filtred['hdi_index'].mean():.2f}")
+c2.metric(label=labelc2, value=f"{round(df_filtred['gdp'].mean(), 2):,}")
 
-fig1 = px.scatter(data_frame=df_filtred, x='gdp', y='life_exp', color='continent')
+fig1 = px.scatter(data_frame=df_filtred, x='gdp', y='life_exp', color='continent', 
+                  title=title)
+# Atualizando o tamanho dos pontos
+fig1.update_traces(marker=dict(size=8))
 # Atualizando as labels dos eixos X e Y
 st.write(fig1.update_layout(
     xaxis_title="PIB per capita",  
-    yaxis_title="Expectativa de Vida" 
+    yaxis_title="Expectativa de Vida"
 ))
+
+# Gráfico 
 
 # Rodapé
 st.sidebar.divider()
